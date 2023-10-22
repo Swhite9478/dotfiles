99d7cbc9c2af51a09bdf74bae81deb24397c2574
Stephen <stevewhite.dev@gmail.com>
2023-10-22 13:28:38 -0700

    finalizing some smaller config changes before moving on to oh-my-zsh configs

--- .config/nvim/lua/martian/plugins/true-zen.lua
+++ .config/nvim/lua/martian/plugins/true-zen.lua
@@ -47,7 +47,7 @@ return {
 					ignored_buf_types = { "nofile" }, -- save current options from any window except ones displaying these kinds of buffers
 					options = { -- options to be disabled when entering Minimalist mode
 						number = false,
-						relativenumber = true,
+						relativenumber = false,
 						showtabline = 0,
 						signcolumn = "no",
 						statusline = "",
