fa769013aa79eab241fe590bf73fc514959eda3a
Stephen <stevewhite.dev@gmail.com>
2023-10-25 13:27:55 -0700

    finally fixed the colorscheme... it was the opacity messing with things

--- .config/zsh/public/exports.sh
+++ .config/zsh/public/exports.sh
@@ -5,3 +5,6 @@ export CONFIG_DIR="$HOME/.config"
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+export PATH="$PATH:$HOME/.cargo/env"
+export PATH="$PATH:$HOME/bin"
